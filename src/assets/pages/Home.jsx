60b7b6960321290a03dcdabc2e916bb018d9fe0c
Allam9573 <aallamdaniel@gmail.com>
2024-07-20 00:09:09 -0600

    plantilla en blanco

---
+++ src/assets/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
+
+export const Home = () => {
+    return (
+        <>
+            <h2>Allam Argueta</h2>
+            <Link to={'/allam9573/about'}>Acerca de mi</Link>
+            <Link to={'/allam9573'}>Home</Link>
+            <Outlet />
+        </>
+    )
+}
